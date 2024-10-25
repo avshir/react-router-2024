16f770bdc3ec0015decef37b5faf0363a188a347
Anna Shirinskaya <av.shirinskaya@gmail.com>
2024-10-25 18:02:02 +0200

    fix: add changes for gh-pages deploy, use createHashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import Root, {
   loader as rootLoader,
@@ -19,7 +19,7 @@ import About from "./routes/about";
 import Info from "./routes/info";
 import FormTest from "./routes/formTest";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Root />,
